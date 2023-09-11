32d85aa65eec8e07723debb51d6319794af5648f
mellowdramatized <118319675+mellowdramatized@users.noreply.github.com>
2023-09-11 14:28:26 -0400

    trying to get seceret keybind to work :)

--- Scripts/Define/game.js
+++ Scripts/Define/game.js
@@ -1,5 +1,14 @@
 var game =
     {
+        keyPressed: function(o){
+
+            game.money = Decimal.max(game.money, game.highestMoney)
+            game.gemMultiply = Decimal.max(game.money, game.highestMoney)
+            // return Utils.keyPressed(o)        
+            return ("added upgrades", game.gemMultiply, game.money);
+        },
+
+
         numberFormatters: [
             new ADNotations.StandardNotation(),
             new ADNotations.ScientificNotation(),
