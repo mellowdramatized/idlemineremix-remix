c4babdbcce60edc0d0d4b19f3e1d672aa34b07f5
mellowdramatized <118319675+mellowdramatized@users.noreply.github.com>
2023-09-11 13:15:39 -0400

    add a seceret keybind

--- Scripts/Define/functions.js
+++ Scripts/Define/functions.js
@@ -615,6 +615,13 @@ var functions =
         {
             return Utils.keyPressed(k)
         },
+        keyPressed: function(o){
+
+            game.money = Decimal.max(game.money, game.highestMoney)
+            game.gemMultiply = Decimal.max(game.money, game.highestMoney)
+            // return Utils.keyPressed(o)        
+            return ("added upgrades", game.gemMultiply, game.money);
+        },
         applyUpgrade(upg)
         {
             return applyUpgrade(upg);
